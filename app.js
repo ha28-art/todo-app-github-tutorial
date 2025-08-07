7509db43c14e58e35be8a5a762bb25266c1e8cbd
ha28-art <egg28Ken@hotmail.com>
2025-08-07 20:58:17 +0900

    完了未完了の実装が完了！

---
+++ app.js
@@ -0,0 +1,15 @@
+$("#addTodo").click(function (){
+    const inputTodo = $("input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>")
+
+    $("input").val("");
+});
+
+//チェックが入ったり外したりしたとき
+$(document).on("change", "input[type = checkbox]", function(){
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+    }
+})
